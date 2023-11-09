66def4b47df4dc76a97ff86f11061bec0a707408
alopvaz <alopvaz014@iesalmudeyne.es>
2023-11-09 14:15:47 +0100

    Seguimos testeando el etiquetado de versiones

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,7 +1,11 @@
-
+/**
+ * Muestra por pantalla la frase "Hola mundo"
+ */
 
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola mundo!");
+        System.out.println("Aqui estamos haciendo pruebas en Git");
+
     }
 }
